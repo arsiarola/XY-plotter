89340ce9a59056d71a2ef17fa47dfa6e30a8591d
ducmavuong <ducvoty@gmail.com>
2020-10-12 00:49:08 +0300

    updating Laser class

--- XY-plotter/src/Laser.h
+++ XY-plotter/src/Laser.h
@@ -8,7 +8,7 @@
 #ifndef LASER_H_
 #define LASER_H_
 
-#if defined (__USE_LPCOPEN)
+#if defined(__USE_LPCOPEN)
 #if defined(NO_BOARD_LIB)
 #include "chip.h"
 #include "sct_15xx.h"
@@ -20,16 +20,17 @@
 #define PWM_FREQ 1000
 #define PWM_CYCLE 255
 
-class Laser {
+class Laser
+{
 public:
     Laser(int port = 0, int pin = 12);
     virtual ~Laser();
+    void setLaser(int pow);
+
 private:
     int port;
     int pin;
+    uint8_t m_power;
 };
-
-
-
 
 #endif /* LASER_H_ */
